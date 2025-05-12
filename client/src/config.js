54d58d6c5612c49cbc9123f86b529d77a4f77091
Visionnaire1000 <cheruiyotlouis@gmail.com>
2025-05-12 22:37:55 +0300

    Update config.js to point to backend on Render

--- client/src/config.js
+++ client/src/config.js
@@ -1,7 +1,8 @@
 // src/config.js
 
 const config = {
-    API_BASE_URL: 'http://localhost:5000',
+    //API_BASE_URL: 'http://localhost:5000',
+    API_BASE_URL: 'https://farmart-app-server.onrender.com',
   };
   
   export default config;
